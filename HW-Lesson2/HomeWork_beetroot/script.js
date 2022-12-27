b74ee1c13c4715d22052ea5186c46e53902d8cb7
Makar90 <Makarlancer@gmail.com>
2022-12-27 17:58:18 +0200

    HW Lesson2

--- HW-Lesson2/HomeWork_beetroot/script.js
+++ HW-Lesson2/HomeWork_beetroot/script.js
@@ -1,14 +1,9 @@
 console.log('External script commited');
-
+//Task Minimum 1
 function sum(a, b) {
     let res = parseFloat(a) + parseFloat(b);
     return res;
 }
-
-function toNumber(a) {
-    return Number(a.replace(/[^0-9.]/g, ""));
-}
-
 let btn1 = document.querySelector('.btn-1');
 
 btn1.onclick = () => {
@@ -20,6 +15,11 @@ btn1.onclick = () => {
     console.log('btn-1 click');
 }
 
+//Task Minimum 2
+function toNumber(a) {
+    return Number(a.replace(/[^0-9.]/g, ""));
+}
+
 let btn2 = document.querySelector('.btn-2');
 btn2.onclick = () => {
     let out = document.querySelector('.out-2');
@@ -29,6 +29,7 @@ btn2.onclick = () => {
     console.log('btn-2 click');
 }
 
+//Task Minimum 3
 let btn3 = document.querySelector('.btn-3');
 btn3.onclick = () => {
     let out = document.querySelector('.out-3');
@@ -41,6 +42,8 @@ btn3.onclick = () => {
     console.log('btn-3 click');
 }
 
+
+//Task Medium 1
 let btn4 = document.querySelector('.btn-4');
 btn4.onclick = () => {
     let out1 = document.querySelector('.out-4-1');
@@ -57,6 +60,7 @@ btn4.onclick = () => {
     console.log('btn-4 click');
 }
 
+//Task Medium 2
 let btn5 = document.querySelector('.btn-5');
 btn5.onclick = () => {
     let out = document.querySelector('.out-5-1');
@@ -70,5 +74,46 @@ btn5.onclick = () => {
     out.value += inp;
     console.log('btn-5 click');
 }
+
+
+//Task Hard 1
+let btn6 = document.querySelector('.btn-6');
+btn6.onclick = () => {
+    let out = document.querySelector('.out-6-1');
+    //out.value = '';
+    //document.querySelector('.out-5-1') = '';
+    let inp1 = document.querySelector('.inp-6-1');
+    let inp2 = document.querySelector('.inp-6-2');
+    let inp3 = document.querySelector('.inp-6-3');
+    out.value = inp1.value * ((inp2.value / 12) * inp3.value) / 100;
+    console.log('btn-6 click');
+}
+
+//Task Hard 2
+let act7_1 = document.querySelector('.act-7-1');
+let act7_2 = document.querySelector('.act-7-2');
+let act7_3 = document.querySelector('.act-7-3');
+let out7_1 = document.querySelector('.out-7-1');
+let out7_2 = document.querySelector('.out-7-2');
+let out7_3 = document.querySelector('.out-7-3');
+
+function conditionOut(con, out) {
+    if (eval(con.textContent))
+        out.value = true;
+    else
+        out.value = false;
+}
+
+act7_1.onclick = () => {
+    conditionOut(act7_1, out7_1);
+}
+act7_2.onclick = () => {
+    conditionOut(act7_2, out7_2);
+}
+act7_3.onclick = () => {
+    conditionOut(act7_3, out7_3);
+}
+
+
 
 //
