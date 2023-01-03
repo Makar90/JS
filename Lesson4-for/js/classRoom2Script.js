e49b29861daedfb432e25cdafe8d6dfea41fdfec
Makar90 <Makarlancer@gmail.com>
2023-01-03 20:27:51 +0200

    HW Lesson4

--- Lesson4-for/js/classRoom2Script.js
+++ Lesson4-for/js/classRoom2Script.js
@@ -4,6 +4,8 @@ let polyInp2 = document.querySelector('.polyInp2');
 let inp2 = document.querySelector('.inp2');
 let out2 = document.querySelector('.out2');
 let btn2 = document.querySelector('.btn2');
+//polyInp2.selectedIndex = -1;
+//polyInp2.value = "Вибери користувача";
 
 polyInp2.onchange = () => {
     out2.innerHTML = '';
@@ -14,12 +16,31 @@ polyInp2.onchange = () => {
     } else {
         out2.innerHTML += `Ви не ввели число!`;
     }
+    /* if (polyInp2.selectedIndex == -1) {
+        out2.innerHTML += " Вибери користувача";
+    } */
 }
 
 btn2.onclick = () => {
     out2.innerHTML = '';
     inp2.value = "";
-    polyInp2.selectedIndex = 0;
+    polyInp2.selectedIndex = -1;
+}
+
+
+
+let out3 = document.querySelector('.out3');
+let btn3 = document.querySelector('.btn3');
+let inp3_1 = document.querySelector('.inp3_1').value;
+let inp3_2 = document.querySelector('.inp3_2').value;
+
+btn3.onclick = () => {
+    for (let i = 0; i < inp3_1; i++) {
+        for (let j = i; j < inp3_2; j++) {
+            out3.innerHTML += `*`;
+        }
+        out3.innerHTML += `<br>`;
+    }
 }
 
 
