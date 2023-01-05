69916677ae33a2bf915224e0b1fe1737cb823fee
Makar90 <Makarlancer@gmail.com>
2023-01-05 19:12:47 +0200

    HW Lesson6

---
+++ Lesson4-for/js/lesson6/classRoom.js
@@ -0,0 +1,17 @@
+console.log('JS Pattern commited');
+/* 
+//Commit external script
+function include(url) {
+    var script = document.createElement('script');
+    script.src = url;
+    document.getElementsByTagName('body')[0].appendChild(script);
+}
+include("../../js/main.js"); */
+
+/* let pointerHeader = document.querySelector('.header');
+includeHTMLHeader(pointerHeader); */
+
+/* let pointerFooter = document.querySelector('.footer');
+includeHTMLFooter(pointerFooter); */
+
+
