7d2f4510994aca98ef701c60c558a137ff3526ba
Makar90 <Makarlancer@gmail.com>
2023-01-07 14:16:43 +0200

    Lesson7

--- Lesson4-for/js/lesson_PatternJS/classRoom.js
+++ Lesson4-for/js/lesson_PatternJS/classRoom.js
@@ -15,3 +15,7 @@ includeHTMLHeader(pointerHeader); */
 includeHTMLFooter(pointerFooter); */
 
 
+let inp = document.querySelector('.inp1_1');
+let out = document.querySelector('.out1_1');
+let btn1 = document.querySelector('.btn1_1');
+
--- Lesson4-for/js/lesson_PatternJS/homework.js
+++ Lesson4-for/js/lesson_PatternJS/homework.js
@@ -1,3 +1,7 @@
 console.log('JS Pattern commited');
 
 
+let inp = document.querySelector('.inp1_1');
+let out = document.querySelector('.out1_1');
+let btn1 = document.querySelector('.btn1_1');
+
