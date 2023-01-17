b17253c94e6c2f8f20543363e5da0c1a06c93f6d
Makar90 <Makarlancer@gmail.com>
2023-01-17 21:01:09 +0200

    Lesson 10(35)

---
+++ Lesson4-for/js/lesson10_35_Events/classRoom.js
@@ -0,0 +1,17 @@
+console.log('JS Pattern commited 11 (35)');
+/* 
+//Commit external script
+function include(url) {
+    var script = document.createElement('script');
+    script.src = url;
+    document.getElementsByTagName('body')[0].appendChild(script);
+}
+include("../../js/main.js"); */
+
+
+
+
+let inp = document.querySelector('.inp1_1');
+let out = document.querySelector('.out1_1');
+let btn1 = document.querySelector('.btn1_1');
+
